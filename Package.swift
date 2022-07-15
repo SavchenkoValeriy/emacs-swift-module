c7a43f2dff607c9082cf88f5d0f05fd89c2c1666
Valeriy Savchenko <sinmipt@gmail.com>
2022-07-15 17:48:11 +0100

    WIP: try fixing inability to use this package in other projects

--- Package.swift
+++ Package.swift
@@ -7,22 +7,13 @@ let package = Package(
   name: "EmacsSwiftModule",
   platforms: [.macOS(.v10_15)],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
-    .library(
-      name: "EmacsSwiftModule",
-      targets: ["EmacsSwiftModule"]),
     .library(
       name: "TestModule",
       type: .dynamic,
       targets: ["TestModule"])
   ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-  ],
+  dependencies: [],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(
       name: "EmacsSwiftModule",
       dependencies: ["EmacsModule"],
