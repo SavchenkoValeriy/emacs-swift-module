c8298845d5079ec2f7723cee88cb1a955163f37f
Valeriy Savchenko <sinmipt@gmail.com>
2022-08-18 17:24:32 +0100

    Add GPL3 comment to each source file

--- Source/Swift/Environment.swift
+++ Source/Swift/Environment.swift
@@ -1,3 +1,22 @@
+//
+// Environment.swift
+// Copyright (C) 2022 Valeriy Savchenko
+//
+// This file is part of EmacsSwiftModule.
+//
+// EmacsSwiftModule is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// EmacsSwiftModule is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// EmacsSwiftModule. If not, see <https://www.gnu.org/licenses/>.
+//
 import EmacsModule
 
 /// Environment is the interaction point with Emacs. If you want to do anything on the Emacs side, you need to have an Environment.
--- Source/Swift/Value.swift
+++ Source/Swift/Value.swift
@@ -1,3 +1,22 @@
+//
+// Value.swift
+// Copyright (C) 2022 Valeriy Savchenko
+//
+// This file is part of EmacsSwiftModule.
+//
+// EmacsSwiftModule is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// EmacsSwiftModule is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// EmacsSwiftModule. If not, see <https://www.gnu.org/licenses/>.
+//
 /// An opaque Emacs value representing something from the Emacs Lisp world.
 ///
 /// Please, don't assume anything based on this object and treat it as a
