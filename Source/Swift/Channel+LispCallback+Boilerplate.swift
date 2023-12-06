53c92bf668cee48580fa854809a6ec7343ea82b4
Valeriy Savchenko <sinmipt@gmail.com>
2023-12-06 17:46:41 +0000

    Simplify channel callbacks

--- Source/Swift/Channel+LispCallback+Boilerplate.swift
+++ Source/Swift/Channel+LispCallback+Boilerplate.swift
@@ -17,86 +17,6 @@
 // You should have received a copy of the GNU General Public License along with
 // EmacsSwiftModule. If not, see <https://www.gnu.org/licenses/>.
 //
-class LazyLispCallback0: AnyLazyCallback {
-  let function: EmacsValue
-  init(function: EmacsValue) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    try env.funcall(function)
-  }
-}
-
-class LazyLispCallback1<T: EmacsConvertible>: AnyLazyCallback {
-  let function: EmacsValue
-  init(function: EmacsValue) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? T {
-      try env.funcall(function, with: arg)
-    }
-  }
-}
-
-class LazyLispCallback2<T1: EmacsConvertible, T2: EmacsConvertible>:
-  AnyLazyCallback
-{
-  let function: EmacsValue
-  init(function: EmacsValue) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? (T1, T2) {
-      try env.funcall(function, with: arg.0, arg.1)
-    }
-  }
-}
-
-class LazyLispCallback3<
-  T1: EmacsConvertible, T2: EmacsConvertible, T3: EmacsConvertible
->: AnyLazyCallback {
-  let function: EmacsValue
-  init(function: EmacsValue) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? (T1, T2, T3) {
-      try env.funcall(function, with: arg.0, arg.1, arg.2)
-    }
-  }
-}
-
-class LazyLispCallback4<
-  T1: EmacsConvertible, T2: EmacsConvertible, T3: EmacsConvertible,
-  T4: EmacsConvertible
->: AnyLazyCallback {
-  let function: EmacsValue
-  init(function: EmacsValue) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? (T1, T2, T3, T4) {
-      try env.funcall(function, with: arg.0, arg.1, arg.2, arg.3)
-    }
-  }
-}
-
-class LazyLispCallback5<
-  T1: EmacsConvertible, T2: EmacsConvertible, T3: EmacsConvertible,
-  T4: EmacsConvertible, T5: EmacsConvertible
->: AnyLazyCallback {
-  let function: EmacsValue
-  init(function: EmacsValue) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? (T1, T2, T3, T4, T5) {
-      try env.funcall(function, with: arg.0, arg.1, arg.2, arg.3, arg.4)
-    }
-  }
-}
-
 extension Channel {
   /// Make a Swift callback out of an Emacs function.
   ///
@@ -109,8 +29,9 @@ extension Channel {
     -> () -> Void
   {
     return { [self] in
-      register(
-        callback: LazyLispCallback0(function: function), args: ())
+      register {
+        env in try env.funcall(function)
+      }
     }
   }
   /// Make a Swift callback out of an Emacs function.
@@ -124,8 +45,9 @@ extension Channel {
     -> (T) -> Void
   {
     return { [self] arg in
-      register(
-        callback: LazyLispCallback1<T>(function: function), args: arg)
+      register {
+        env in try env.funcall(function, with: arg)
+      }
     }
   }
   /// Make a Swift callback out of an Emacs function.
@@ -139,9 +61,9 @@ extension Channel {
     _ function: EmacsValue
   ) -> (T1, T2) -> Void {
     return { [self] (arg1, arg2) in
-      register(
-        callback: LazyLispCallback2<T1, T2>(function: function),
-        args: (arg1, arg2))
+      register {
+        env in try env.funcall(function, with: arg1, arg2)
+      }
     }
   }
   /// Make a Swift callback out of an Emacs function.
@@ -157,9 +79,9 @@ extension Channel {
     _ function: EmacsValue
   ) -> (T1, T2, T3) -> Void {
     return { [self] (arg1, arg2, arg3) in
-      register(
-        callback: LazyLispCallback3<T1, T2, T3>(function: function),
-        args: (arg1, arg2, arg3))
+      register {
+        env in try env.funcall(function, with: arg1, arg2, arg3)
+      }
     }
   }
   /// Make a Swift callback out of an Emacs function.
@@ -176,9 +98,9 @@ extension Channel {
     _ function: EmacsValue
   ) -> (T1, T2, T3, T4) -> Void {
     return { [self] (arg1, arg2, arg3, arg4) in
-      register(
-        callback: LazyLispCallback4<T1, T2, T3, T4>(function: function),
-        args: (arg1, arg2, arg3, arg4))
+      register {
+        env in try env.funcall(function, with: arg1, arg2, arg3, arg4)
+      }
     }
   }
   /// Make a Swift callback out of an Emacs function.
@@ -195,9 +117,9 @@ extension Channel {
     _ function: EmacsValue
   ) -> (T1, T2, T3, T4, T5) -> Void {
     return { [self] (arg1, arg2, arg3, arg4, arg5) in
-      register(
-        callback: LazyLispCallback5<T1, T2, T3, T4, T5>(function: function),
-        args: (arg1, arg2, arg3, arg4, arg5))
+      register {
+        env in try env.funcall(function, with: arg1, arg2, arg3, arg4, arg5)
+      }
     }
   }
 }
--- Source/Swift/Channel+SwiftCallback+Boilerplate.swift
+++ Source/Swift/Channel+SwiftCallback+Boilerplate.swift
@@ -17,76 +17,6 @@
 // You should have received a copy of the GNU General Public License along with
 // EmacsSwiftModule. If not, see <https://www.gnu.org/licenses/>.
 //
-class LazySwiftCallback0: AnyLazyCallback {
-  let function: (Environment) throws -> Void
-  init(function: @escaping (Environment) throws -> Void) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    try function(env)
-  }
-}
-
-class LazySwiftCallback1<T>: AnyLazyCallback {
-  let function: (Environment, T) throws -> Void
-  init(function: @escaping (Environment, T) throws -> Void) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? T {
-      try function(env, arg)
-    }
-  }
-}
-
-class LazySwiftCallback2<T1, T2>: AnyLazyCallback {
-  let function: (Environment, T1, T2) throws -> Void
-  init(function: @escaping (Environment, T1, T2) throws -> Void) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? (T1, T2) {
-      try function(env, arg.0, arg.1)
-    }
-  }
-}
-
-class LazySwiftCallback3<T1, T2, T3>: AnyLazyCallback {
-  let function: (Environment, T1, T2, T3) throws -> Void
-  init(function: @escaping (Environment, T1, T2, T3) throws -> Void) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? (T1, T2, T3) {
-      try function(env, arg.0, arg.1, arg.2)
-    }
-  }
-}
-
-class LazySwiftCallback4<T1, T2, T3, T4>: AnyLazyCallback {
-  let function: (Environment, T1, T2, T3, T4) throws -> Void
-  init(function: @escaping (Environment, T1, T2, T3, T4) throws -> Void) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? (T1, T2, T3, T4) {
-      try function(env, arg.0, arg.1, arg.2, arg.3)
-    }
-  }
-}
-
-class LazySwiftCallback5<T1, T2, T3, T4, T5>: AnyLazyCallback {
-  let function: (Environment, T1, T2, T3, T4, T5) throws -> Void
-  init(function: @escaping (Environment, T1, T2, T3, T4, T5) throws -> Void) {
-    self.function = function
-  }
-  func call(_ env: Environment, with args: Any) throws {
-    if let arg = args as? (T1, T2, T3, T4, T5) {
-      try function(env, arg.0, arg.1, arg.2, arg.3, arg.4)
-    }
-  }
-}
-
 extension Channel {
   /// Make a callback that doesn't require the environment from a closure that does.
   ///
@@ -99,8 +29,9 @@ extension Channel {
     -> () -> Void
   {
     return { [self] in
-      register(
-        callback: LazySwiftCallback0(function: function), args: ())
+      register {
+        env in try function(env)
+      }
     }
   }
   /// Make a callback that doesn't require the environment from a closure that does.
@@ -114,8 +45,9 @@ extension Channel {
     -> (T) -> Void
   {
     return { [self] arg in
-      register(
-        callback: LazySwiftCallback1(function: function), args: arg)
+      register {
+        env in try function(env, arg)
+      }
     }
   }
   /// Make a callback that doesn't require the environment from a closure that does.
@@ -129,8 +61,9 @@ extension Channel {
     function: @escaping (Environment, T1, T2) throws -> Void
   ) -> (T1, T2) -> Void {
     return { [self] (arg1, arg2) in
-      register(
-        callback: LazySwiftCallback2(function: function), args: (arg1, arg2))
+      register {
+        env in try function(env, arg1, arg2)
+      }
     }
   }
   /// Make a callback that doesn't require the environment from a closure that does.
@@ -144,9 +77,9 @@ extension Channel {
     function: @escaping (Environment, T1, T2, T3) throws -> Void
   ) -> (T1, T2, T3) -> Void {
     return { [self] (arg1, arg2, arg3) in
-      register(
-        callback: LazySwiftCallback3(function: function),
-        args: (arg1, arg2, arg3))
+      register {
+        env in try function(env, arg1, arg2, arg3)
+      }
     }
   }
   /// Make a callback that doesn't require the environment from a closure that does.
@@ -160,9 +93,9 @@ extension Channel {
     function: @escaping (Environment, T1, T2, T3, T4) throws -> Void
   ) -> (T1, T2, T3, T4) -> Void {
     return { [self] (arg1, arg2, arg3, arg4) in
-      register(
-        callback: LazySwiftCallback4(function: function),
-        args: (arg1, arg2, arg3, arg4))
+      register {
+        env in try function(env, arg1, arg2, arg3, arg4)
+      }
     }
   }
   /// Make a callback that doesn't require the environment from a closure that does.
@@ -176,9 +109,9 @@ extension Channel {
     function: @escaping (Environment, T1, T2, T3, T4, T5) throws -> Void
   ) -> (T1, T2, T3, T4, T5) -> Void {
     return { [self] (arg1, arg2, arg3, arg4, arg5) in
-      register(
-        callback: LazySwiftCallback5(function: function),
-        args: (arg1, arg2, arg3, arg4, arg5))
+      register {
+        env in try function(env, arg1, arg2, arg3, arg4, arg5)
+      }
     }
   }
 
@@ -192,7 +125,9 @@ extension Channel {
   public func withEnvironment(
     _ function: @escaping (Environment) throws -> Void
   ) {
-    register(callback: LazySwiftCallback0(function: function), args: ())
+    register {
+      env in try function(env)
+    }
   }
 
   /// Execute the given closure with Emacs environment and return its result.
