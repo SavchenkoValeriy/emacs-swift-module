73bca272aaf972f640d6c40777fd06eaf99f43b3
Valeriy Savchenko <sinmipt@gmail.com>
2023-12-17 08:58:11 +0000

    Add test for defun with captures and its finalizer

--- test/Unit/DefunTests.swift
+++ test/Unit/DefunTests.swift
@@ -16,4 +16,30 @@ class DefunTests: XCTestCase {
     try env.funcall("trivial")
     XCTAssert(called)
   }
+
+  func testFunctionFinalizer() throws {
+    let mock = EnvironmentMock()
+    let env = mock.environment
+
+    var accumulator = ""
+
+    // We need this closure capture and keep some piece
+    // of data alive, so that if we don't clean it up, it
+    // would end up in a leak.
+    try env.defun("add") {
+      (str: String) in accumulator += str
+    }
+
+    try env.funcall("add", with: "Hello")
+    XCTAssertEqual(accumulator, "Hello")
+
+    try env.funcall("add", with: ", ")
+    XCTAssertEqual(accumulator, "Hello, ")
+
+    try env.funcall("add", with: "World")
+    XCTAssertEqual(accumulator, "Hello, World")
+
+    try env.funcall("add", with: "!")
+    XCTAssertEqual(accumulator, "Hello, World!")
+  }
 }
