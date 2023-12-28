b70ebc30c4204f17e6217f2fed257609c4f139d9
Valeriy Savchenko <sinmipt@gmail.com>
2023-12-28 15:58:32 +0000

    Add header license comments to all tests

--- test/Unit/LeaksTests.swift
+++ test/Unit/LeaksTests.swift
@@ -1,3 +1,22 @@
+//
+// LeaksTests.swift
+// Copyright (C) 2022-2023 Valeriy Savchenko
+//
+// This file is part of EmacsSwiftModule.
+//
+// EmacsSwiftModule is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// EmacsSwiftModule is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// EmacsSwiftModule. If not, see <https://www.gnu.org/licenses/>.
+//
 import XCTest
 
 // Only build when built through SPM, as tests run through Xcode don't like this.
